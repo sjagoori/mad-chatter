2506650a81ca890a9e9befb1633375ed338ff69c
Jochem Vogel <voge0031@hz.nl>
2021-04-06 13:02:14 +0200

    Add starting chat functionality

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
 const io = require('socket.io')(http);
+const port = process.env.PORT || 4000;
 
 app.use(express.static(path.resolve('public')));
 
 io.on('connection', (socket) => {
-  console.log('user connected')
+  console.log('user connected');
+
+  socket.on('message', (messageText) => {
+    console.log('message is: ', messageText);
+  });
+
+  socket.on('typing', (status) => {
+    socket.emit('typing', status);
+    console.log('typing: \t', status);
+  });
+
   socket.on('disconnect', () => {
     console.log('disconnected');
-  })
-})
+  });
+});
 
 http.listen(port, () => {
   console.log(`listening to port ${port}`);
-})
+});
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,49 @@
-console.log('script running');
 let socket = io();
+const messages = document.querySelector('#chat ol');
+const input = document.getElementById('message');
+const form = document.getElementsByTagName('form')[0];
+const typingText = document.getElementById('typing');
 
+let typing = false;
+let typingTimeout = undefined;
+var startTyping;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let value = e.target[0].value;
+  if (value) socket.emit('message', value), (value = '');
+});
+
+form.addEventListener('keyup', () => {
+  const isTypingTimeoutDuration = 3000;
+  clearTimeout(typingTimeout);
+
+  typingText.hidden = false;
+
+  typing = true;
+  socket.emit('typing', typing);
+
+  typingTimeout = setTimeout(() => {
+    typing = false;
+    typingText.hidden = true;
+    socket.emit('typing', typing);
+  }, isTypingTimeoutDuration);
+});
+
+socket.on('message', (emitted) => {
+  console.log(emitted);
+  // Display message in the chat
+  let newMessage = document.createElement('li');
+  newMessage.textContent = emitted;
+  messages.appendChild(newMessage);
+});
+
+socket.on('typing', (emitted) => {
+  console.log(emitted);
+  let statusString = document.getElementsByTagName('small')[0];
+  let name = (statusString.children[0].setAttribute = 'block');
+});
+
+socket.on('connected', (e) => {
+  socket.emit('status', 'online');
+});
