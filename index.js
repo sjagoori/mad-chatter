beb841c4c1412188413fc46fc7837fd6841f9c56
Jochem Vogel <voge0031@hz.nl>
2021-04-09 13:34:30 +0200

    Remove comment

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ io.on('connection', (socket) => {
 
   socket.on('buzzer', () => {
     io.emit('buzzer');
-    // socket.broadcast.emit('buzzer');
   });
 
   socket.emit('onlineCount', io.engine.clientsCount);
