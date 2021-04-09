c157175c0d13d533997fce831fc596d03cb12cba
Jochem Vogel <voge0031@hz.nl>
2021-04-09 13:26:19 +0200

    Remove broadcast emit

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ io.on('connection', (socket) => {
 
   socket.on('buzzer', () => {
     socket.emit('buzzer');
-    socket.broadcast.emit('buzzer');
   });
 
   socket.emit('onlineCount', io.engine.clientsCount);
